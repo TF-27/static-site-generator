68f3031ccf11e9dd3711f844e571574a53557dcd
TF-27 <jeroenavn@proton.me>
2026-04-30 18:50:59 +0200

    modified for github site hosting

--- src/main.py
+++ src/main.py
@@ -3,14 +3,17 @@ import os
 import shutil
 from markdown_blocks import markdown_to_html_node
 import pathlib
+import sys
 
 def main():
-    copy_static_to_main("static", "public")
-    generate_pages_recursive("content","template.html","public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_log = copy_static_to_main("static", "docs")
+    generate_pages_recursive("content","template.html","docs", basepath)
+    print(f"file copy log:\n{copy_log}")
 
 
 def copy_static_to_main(content_path, destination_path):
-    shutil.rmtree(destination_path)
+    shutil.rmtree(destination_path, ignore_errors=True)
     os.mkdir(destination_path)
     file_log = {"new_directory": [], "new_file": []}
     for item in os.listdir(content_path):
@@ -23,7 +26,7 @@ def copy_static_to_main(content_path, destination_path):
             os.mkdir(item_destination)
             file_log["new_directory"].append(item_destination)
             copy_static_to_main(item_path, item_destination)
-    print(file_log)
+    return file_log
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -33,7 +36,7 @@ def extract_title(markdown):
             return title[1].strip()
     raise Exception("No header provided")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"\nGenerating page from {from_path} to {dest_path} using {template_path}\n")
     with open(from_path, "r") as f:
         file = f.read()
@@ -44,6 +47,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(file)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_string)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     #print(template)
 
     if os.path.dirname(dest_path):
@@ -51,16 +56,16 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     content = os.listdir(dir_path_content)
     for item in content:
         item_path = os.path.join(dir_path_content, item)
-        print(f"ITEM PATH: {item_path}")
+        #print(f"ITEM PATH: {item_path}")
         if os.path.isfile(item_path):
             if pathlib.Path(item_path).suffix == ".md":
-                generate_page(item_path, template_path, os.path.join(dest_dir_path, item.replace(".md", ".html")))
+                generate_page(item_path, template_path, os.path.join(dest_dir_path, item.replace(".md", ".html")), basepath)
         elif os.path.isdir(item_path):
-            generate_pages_recursive(item_path, template_path, os.path.join(dest_dir_path, item))
+            generate_pages_recursive(item_path, template_path, os.path.join(dest_dir_path, item), basepath)
     
 
 
